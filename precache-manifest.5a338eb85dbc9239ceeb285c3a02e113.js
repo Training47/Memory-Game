2fde8a80b8e383acd41adac099217c560c0ec4b7
Lisa Pena <lisapena13@gmail.com>
2020-09-19 15:48:58 -0500

    Updates

---
+++ precache-manifest.5a338eb85dbc9239ceeb285c3a02e113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f757c4023efafa3b27a99bf22e1604",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "90487b181fe6d2766cec",
+    "url": "/Memory-Game/static/css/main.5aa74263.chunk.css"
+  },
+  {
+    "revision": "689649a47605e069e48a",
+    "url": "/Memory-Game/static/js/2.a3fab9b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Memory-Game/static/js/2.a3fab9b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90487b181fe6d2766cec",
+    "url": "/Memory-Game/static/js/main.2d0a80d7.chunk.js"
+  },
+  {
+    "revision": "a620562155869f915d9a",
+    "url": "/Memory-Game/static/js/runtime-main.0da77a42.js"
+  }
+]);
